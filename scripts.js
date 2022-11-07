e6a0a7d279112f5d0d8394e1b1278a1f0ef79c1a
ayanraidhasan <110635638+ayanraidhasan@users.noreply.github.com>
2022-11-07 18:39:06 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-//Button Interactivity + Modal
-
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
